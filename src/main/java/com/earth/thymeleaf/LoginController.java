b86fe9cc20418ac093c7e2d1e4e72f01e6cc211e
chjin <seedevk8s@gmail.com>
2023-06-14 01:17:51 +0900

    forward와 redirect 테스트 내용 추가

--- src/main/java/com/earth/thymeleaf/LoginController.java
+++ src/main/java/com/earth/thymeleaf/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -24,7 +25,7 @@ public class LoginController {
     // 하나의 메서드로 GET, POST 둘다 처리하는 경우
     //@RequestMapping(value = "/login/login", method = {RequestMethod.GET, RequestMethod.POST})
     @PostMapping("/login")
-    public String login(String id, String pwd, RedirectAttributes model) throws UnsupportedEncodingException {
+    public String login(HttpServletRequest request, String id, String pwd, RedirectAttributes model) throws UnsupportedEncodingException {
         // 1. id, pwd를 확인
         if (loginCheck(id, pwd)) {
             // 2. 일치하면, userInfo.html
@@ -36,7 +37,12 @@ public class LoginController {
             //    일치하지 않으면, login.html로 이동
             String msg = "id 또는 pwd가 일치하지 않습니다.";
             model.addAttribute("msg", msg);
-            return "redirect:/login/login";        //GET 요청 - URL 재작성
+            model.addFlashAttribute("msg", "일회용 메시지");  //session에 잠깐 저장되었다가 삭제됨
+            request.setAttribute("msg", "request에 저장된 msg");
+
+            return "forward:/";
+            //return "forward:/login/login";          // /login/login POST 요청임 => 이렇게되면 무한루프가 됨
+            //return "redirect:/login/login?msg="+msg;        //GET 요청 - URL 재작성
         }
 
     }
